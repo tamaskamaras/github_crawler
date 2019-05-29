789501c71e40786568d23469bf4d59dc306a65b6
Tamás Kamarás <kamaras.tamas@ejogseged.hu>
2019-05-29 17:12:05 +0200

    get multiple patches

--- app/lib/api_crawler.rb
+++ app/lib/api_crawler.rb
@@ -1,30 +1,30 @@
 class ApiCrawler
 
-	attr_reader :user, :repo, :github_client
+  attr_reader :user, :repo, :github_client
 
-	def initialize attributes
-		@user 				 = attributes[:user]
-		@repo 				 = attributes[:repo]
-		@github_client = Github.new(basic_auth: "#{attributes[:login]}:#{attributes[:password]}")
-	end
+  def initialize attributes
+    @user 				 = attributes[:user]
+    @repo 				 = attributes[:repo]
+    @github_client = Github.new(basic_auth: "#{attributes[:login]}:#{attributes[:password]}")
+  end
 
-	def filtered_pull_request
-		result = {}
-		pull_requests do |pull_request|
-			file_attributes = {}
+  def filtered_pull_request
+    result = {}
+    pull_requests do |pull_request|
+      file_attributes = {}
       pull_request_files(pull_request['number']) do |file|
-				file_name = file['filename']
-        (file_attributes[file_name] || file_attributes[file_name] = {
+        file_path = file['filepath']
+        (file_attributes[file_path] || file_attributes[file_path] = {
           blob_url: file['blob_url'],
-          patches:  []
-        })[:patches] << file['patch'][/\A@@ (.*) @@/, 1]
-			end
-			if file_urls = redundant_file_urls(file_attributes)
-				result.merge({ [ pull_request['url'] ] => file_urls })
-			end
-		end
-		result
-	end
+          line_ranges:  []
+        })[:line_ranges].concat( patchs_to_ranges( file['patch'] ) )
+      end
+      if file_urls = redundant_file_urls(file_attributes)
+        result.merge({ [ pull_request['url'] ] => file_urls })
+      end
+    end
+    result
+  end
 
   def pull_requests
     github_client.pull_requests.list(user: user, repo: repo) do |pull_request|
@@ -42,16 +42,25 @@ class ApiCrawler
     end
   end
 
-	def redundant_file_urls file_attributes
-		result = []
-		file_attributes.each do |filename, attributes|
-			if (line_numbers = attributes[:patches]).length > 1
-				line_numbers.each do |lineno|
-					lineno[/\A-(.*) /, 1].split(',')
-				end
-			end
-		end
-		result
-	end
+  def patchs_to_ranges patches
+    patches.split(%r{@@ -}).map do |patch|
+      if raw_range = patch[/\A\d+,\d+/]&.split(',')
+        start = raw_range[0].to_i
+        Range.new(start, start + raw_range[1].to_i)
+      end
+    end.compact
+  end
+
+  def redundant_file_urls file_attributes
+    file_attributes.inject([]) do |file_urls, (file_path, attributes)|
+      file_urls << attributes[:blob_url] if overlap_ranges( attributes[:line_ranges] )
+      file_urls
+    end
+  end
+
+  def overlap_ranges ranges
+    ranges.inject([]) do |line_ranges, line_range|
+    end if ranges.length > 1
+  end
 
 end
